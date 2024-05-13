c09814ede5aaa2363f488b52082055d80b0ba9dd
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-05-14 00:18:41 +0800

    sign in, sign out, check auth status

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,16 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
 
 interface ProvidersProprs {
   readonly children: React.ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProprs) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 }
