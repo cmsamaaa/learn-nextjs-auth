81bda51306bf44c3cc077b183f505d571b850be3
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-05-16 00:47:18 +0800

    refactor header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,67 +1,8 @@
 import Link from 'next/link';
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from '@nextui-org/react';
-import { auth } from '@/auth';
-import * as actions from '@/actions';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Input } from '@nextui-org/react';
+import HeaderAuth from '@/components/header-auth';
 
-export default async function Header() {
-  const session = await auth();
-
-  let authContent: React.ReactNode;
-  if (session?.user) {
-    authContent = (
-      <Popover placement='left'>
-        <PopoverTrigger>
-          <Avatar src={session.user.image ?? ''} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className='p-4'>
-            <form action={actions.signOut}>
-              <Button type='submit'>Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button
-              type='submit'
-              color='secondary'
-              variant='bordered'
-            >
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button
-              type='submit'
-              color='primary'
-              variant='flat'
-            >
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
-
+export default function Header() {
   return (
     <Navbar className='mb-6 shadow'>
       <NavbarBrand>
@@ -77,7 +18,9 @@ export default async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify='end'>{authContent}</NavbarContent>
+      <NavbarContent justify='end'>
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
