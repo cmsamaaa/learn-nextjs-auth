ae2a135699b6c044e9973a80a06489eda8c7042e
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-05-26 00:37:40 +0800

    Practice #4 (#4)
    * button loader
    
    * add topics chip
    
    * create post
    
    * update next config
    
    * remove pre-commit temporarily
    
    * merge some template files
    
    * update db
    
    * add back pre-commit

---
+++ src/components/posts/post-show.tsx
@@ -0,0 +1,10 @@
+// interface PostShowProps {}
+
+// export default function PostShow({}: PostShowProps) {
+//   return (
+//     <div className='m-4'>
+//       <h1 className='my-2 text-2xl font-bold'>{post.title}</h1>
+//       <p className='rounded border p-4'>{post.content}</p>
+//     </div>
+//   );
+// }
