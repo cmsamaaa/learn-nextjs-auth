a283370592b6620128a8be22aa1e1013030e0595
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-05-25 23:36:20 +0800

    add topics chip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Divider } from '@nextui-org/react';
 import TopicCreateForm from '@/components/topics/topic-create-form';
+import TopicList from '@/components/topics/topic-list';
 
 export default async function Home() {
   return (
@@ -6,8 +8,11 @@ export default async function Home() {
       <div className='col-span-3'>
         <h1 className='m-2 text-xl'>Top Posts</h1>
       </div>
-      <div>
+      <div className='border px-2 py-3 shadow'>
         <TopicCreateForm />
+        <Divider className='my-2' />
+        <h3 className='text-lg'>Topics</h3>
+        <TopicList />
       </div>
     </div>
   );
